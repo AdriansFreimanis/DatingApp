1ab6534ea1ab99682a6251ee21b2a7b2dc44a849
AdriansFreimanis <adrians.freimanis@gmail.com>
2024-08-16 23:17:35 +0300

    Durring section 5

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { User } from '../_models/user';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  [x: string]: any;
+
+  private http = inject(HttpClient);
+  baseUrl = "https://localhost:5001/api/";
+  currentUser = signal<User | null>(null)
+
+  login(model: any)
+  {
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+     //Signal setup
+      map(user =>{
+        if (user){
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+      })
+    )
+  }
+
+  logout()
+  {
+    localStorage.removeItem('user');
+    this.currentUser.set(null);
+  }
+}
