6587d56d269d626c83a3020f50c5b7dae371c35d
AdriansFreimanis <adrians.freimanis@gmail.com>
2024-08-17 12:01:00 +0300

    End of section 5

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -26,6 +26,20 @@ export class AccountService {
     )
   }
 
+  register(model: any)
+  {
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+     //Signal setup
+      map(user =>{
+        if (user){
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+        return user;
+      })
+    )
+  }
+
   logout()
   {
     localStorage.removeItem('user');
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-register',
@@ -12,13 +13,22 @@ import { FormsModule } from '@angular/forms';
 
 
 export class RegisterComponent {
+
+private accountService = inject(AccountService);
+  cancelRegister = output<boolean>();
   model: any = {}
 
   register(){
-    console.log(this.model);
+    this.accountService.register(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.cancel();
+      },
+      error: error => console.log(error)
+    })
   }
 
   cancel(){
-    console.log('cancelled');
+    this.cancelRegister.emit(false);
   }
 }
