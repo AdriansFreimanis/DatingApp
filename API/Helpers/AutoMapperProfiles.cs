bb471d533f73c5690ee3bf0620c1e8730aa598a6
AdriansFreimanis <adrians.freimanis@gmail.com>
2024-08-26 13:56:09 +0300

    End of Section 8

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -11,7 +11,7 @@ public class AutoMapperProfiles : Profile
     public AutoMapperProfiles()
     {
         CreateMap<AppUser, MemberDTO>()
-        .ForMember(d => d.Age, o => o.MapFrom(s => s.DateOfBirth.CalculateAge()))
+        //.ForMember(d => d.Age, o => o.MapFrom(s => s.DateOfBirth.CalculateAge()))
         .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.GetAge()))
         .ForMember(d => d.PhotoUrl, o => o
             .MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain)!.Url));
